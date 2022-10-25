b78c633d065bb50d0dc612a8d685d50bffea1697
Tajwar Saiyeed Abid <105134106+TajwarSaiyeed@users.noreply.github.com>
2022-10-25 14:48:22 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const cources = require("./data/cources.json");
+const courses = require("./data/courses.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/cources", (req, res) => {
-  res.send(cources);
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find((course) => course._id === id);
+  res.send(selectedCourse);
 });
 
 app.listen(port, () => {
