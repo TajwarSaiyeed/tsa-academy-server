68b82c31664b5fab04fead37882ebc8c59c035ce
Tajwar Saiyeed Abid <105134106+TajwarSaiyeed@users.noreply.github.com>
2022-10-25 08:33:18 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
